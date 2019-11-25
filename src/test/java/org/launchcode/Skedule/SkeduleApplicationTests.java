8d79780ca82b64cd581e80905a4d0e4c9d7e5c76
Maria Vanessa Moreno <MariaVanessaMoreno@outlook.com>
2019-11-25 13:55:33 -0500

    Open Project Folder

---
+++ src/test/java/org/launchcode/Skedule/SkeduleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Skedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
