f59f11f522a2a798becea31093fa1446ce91116b
Maria Vanessa Moreno <MariaVanessaMoreno@outlook.com>
2019-12-04 14:45:51 -0500

    initial controller code

--- src/main/java/org/launchcode/Skedule/controllers/SkeduleController.java
+++ src/main/java/org/launchcode/Skedule/controllers/SkeduleController.java
@@ -1,9 +1,11 @@
 package org.launchcode.Skedule.controllers;
 
-        import org.launchcode.Skedule.models.data.EventsDao;
-        import org.launchcode.Skedule.models.data.UsersDao;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.stereotype.Controller;
+import org.launchcode.Skedule.models.data.EventsDao;
+import org.launchcode.Skedule.models.data.UsersDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SkeduleController {
@@ -14,4 +16,11 @@ public class SkeduleController {
     @Autowired
     private EventsDao eventsDao;
 
+
+
+    @RequestMapping(value = "register", method = RequestMethod.GET)
+    public String main() {
+
+        return "register";
+    }
 }
--- src/main/java/org/launchcode/Skedule/controllers/UsersController.java
+++ src/main/java/org/launchcode/Skedule/controllers/UsersController.java
@@ -1,4 +1,20 @@
 package org.launchcode.Skedule.controllers;
 
+import org.launchcode.Skedule.models.data.UsersDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("users")
 public class UsersController {
+    @Autowired
+    private UsersDao usersDao;
+
+    @RequestMapping(value="register")
+    public String index(Model model) {
+        model.addAttribute("");
+        model.addAttribute("users", usersDao.findAll());
+        return "";
+    }
 }
